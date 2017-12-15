cf72daa5f6ee2d99ce41c6b80cf08500ab97a9f7
Juan Diaz <juandiaz9114@gmail.com>
2017-12-14 21:14:19 -0800

    Edits

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var rightEye;
 var spaceBetweenEyes;
 var widthOfRectangle;
 var positionOfTrack;
+var stunnas = "assets/glasses2.png";
 //check is image is stored in local localStorage if not it sets default
 $(function pictureCheck(){
   if (localStorage.getItem("glassesScan") !== null) {
@@ -42,12 +43,9 @@ $(function pictureCheck(){
     var loadingImage = loadImage(
         e.target.files[0],
         function (img) {
-
-            var canvas = document.getElementById('imageGlasses'),
-                context = canvas.getContext('2d'),
-                overlay = document.getElementById('overlay'),
-                overlayCC = overlay.getContext('2d');
                 overlayCC.clearRect(0,0, overlay.width, overlay.height);
+                overlay.removeAttribute('style');
+                pos = [];
                 context.clearRect(0, 0, canvas.width, canvas.height);
                 context.drawImage(img,canvas.width/2 - img.width/2,canvas.height/2 - img.height/2);
               var imgURL =   canvas.toDataURL();
@@ -59,16 +57,19 @@ $(function pictureCheck(){
     );
     };
 
-var overlay;
-var overlayCC;
+var overlay = document.getElementById('overlay');
+var canvas =  document.getElementById('imageGlasses');
+var context = canvas.getContext('2d');
+var overlayCC = overlay.getContext('2d');;
 var ctrack;
 var drawRequest;
 function canvasSearchMethod(){
   overlay = document.getElementById('overlay');
-  var canvas = document.getElementById('imageGlasses')
+  canvas = document.getElementById('imageGlasses');
   overlayCC = overlay.getContext('2d');
   ctrack = new clm.tracker({stopConvergence : true});
   ctrack.init();
+  overlay.removeAttribute('style');
   return animateClean();
 }
 
@@ -112,30 +113,33 @@ function glassesPositionCanvas(){
 var rightCoors = {
 	x: pos[15][0],
 	y: pos[15][1]};
-
-// angle in radians
-var angleRadians = Math.atan2(rightCoors.y - leftCoors.y, rightCoors.x - leftCoors.x);
-
 // angle in degrees
 var angleDeg = Math.atan2(rightCoors.y - leftCoors.y, rightCoors.x - leftCoors.x) * 180 / Math.PI;
-console.log('degrees: '+angleDeg, 'radians: ' + angleRadians);
   var widthOfRectangle = pos[14][0] - pos[0][0];
   var heightOfRectangle = (pos[41][1]- pos[33][1])* 2;
   console.log("This is the width "+ widthOfRectangle);
   console.log("This is the height "+ heightOfRectangle);
+  overlay.style.webkitTransform = "rotate(" +angleDeg+"deg)";
+  overlay.style.transform = "rotate(" +angleDeg+"deg)";
+  overlay.style.msTransform = "rotate(" +angleDeg+"deg)";
+  //drawGlasses();
+//}
   //creates the image and places it after it has calculated the width of the neccesarry glasses
-
-  var ctx = document.getElementById('overlay').getContext('2d');
-  ctx.clearRect(0,0, document.getElementById('overlay').width, document.getElementById('overlay').height);
+//function drawGlasses(){
+  overlayCC.clearRect(0,0, overlay.width, overlay.height);
+  console.log('it is in the drawGlasses');
   var img = new Image();
-  img.src = "assets/glasses2.png";
+  //console.log('var img');
+  img.src = stunnas;
+  //console.log('img src to glasses' + img.src);
   img.onload = function() {
-
-  ctx.drawImage(img, pos[0][0],pos[19][1],widthOfRectangle, img.height * (widthOfRectangle/img.width));
-  var rotate = document.getElementById('overlay');
+//console.log(img);
+  overlayCC.drawImage(img, pos[0][0],pos[20][1],widthOfRectangle, img.height * (widthOfRectangle/img.width));
+  console.log('drew image');
+  /*var rotate = document.getElementById('overlay');
   rotate.style.webkitTransform = "rotate(" +angleDeg+"deg)";
   rotate.style.transform = "rotate(" +angleDeg+"deg)";
-  rotate.style.msTransform = "rotate(" +angleDeg+"deg)";
+  rotate.style.msTransform = "rotate(" +angleDeg+"deg)";*/
   //attach the glasses as a div element
   // var img = document.getElementById('overlay');
   //var eyes = document.createElement('img');
@@ -146,4 +150,42 @@ console.log('degrees: '+angleDeg, 'radians: ' + angleRadians);
   //img.style.height = "auto";
   //img.style.left = (pos[0][0] ) + 'px';
   //img.style.top = (pos[19][0]) + 'px';
-  };}
+  };
+}
+
+  document.getElementById('eugene').onclick = function(e){
+    stunnas = "assets/EUGENE.png";
+    /*if (pos){
+      drawGlasses();
+    }
+    else{*/
+      canvasSearchMethod();
+    //}
+  }
+  document.getElementById('sailor').onclick = function(e){
+    stunnas = "assets/SAILOR.png";
+    /*if (pos){
+      drawGlasses();
+    }
+    else{*/
+      canvasSearchMethod();
+    //}
+  }
+  document.getElementById('SAINTBARTS').onclick = function(e){
+    stunnas= "assets/SAINTBARTS.png";
+    /*if (!pos){
+      drawGlasses();
+    }
+    else{*/
+      canvasSearchMethod();
+    //}
+  }
+  document.getElementById('sky').onclick = function(e){
+    stunnas= "assets/SKY.png";
+    /*if (!pos){
+      drawGlasses();
+    }
+    else{*/
+      canvasSearchMethod();
+    //}
+  }
